8f1526a4f95401fa2b2a86f3379c05610bd14ca7
cihatkama <cihatkama@gmail.com>
2020-04-11 23:02:14 +0300

    First test code is committed.

---
+++ JavaTraining/src/tr/com/ckama/HelloWorld.java
@@ -0,0 +1,16 @@
+package tr.com.ckama;
+
+/**
+ * 
+ * @author ckama
+ * 
+ *         <h6>This is the first test code.</h6>
+ */
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+
+}
