4788ecdef0361d433b5f26da5a18467adfac4b40
cihatkama <cihatkama@gmail.com>
2020-04-19 22:22:55 +0300

    Weather class updated for Q48

--- HW01/src/forecast/Weather.java
+++ HW01/src/forecast/Weather.java
@@ -4,7 +4,7 @@ public class Weather {
 
 	private static boolean heatWave = true;
 
-	public static void main(String[] args) {
+	public static void main() {
 		boolean heatWave = false;
 		System.out.println("Local heatWave : " + heatWave);
 
