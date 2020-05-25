01f6b7851667485456679650b0b5b4e980ba0c00
cihatkama <cihatkama@gmail.com>
2020-05-25 20:26:22 +0300

    Tested Q08

--- HW06/src/tr/com/ckama/Q08.java
+++ HW06/src/tr/com/ckama/Q08.java
@@ -1,8 +1,13 @@
 package tr.com.ckama;
 
 public class Q08 {
-	Long compute() {
+	static Long compute(int id) {
 		return (long) 10;
+	}
+
+	public static void main(String[] args) {
+		Long x = compute(5);
+		System.out.println(x);
 	}
 
 }
