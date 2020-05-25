82959c2384e413e9641dfdda8807854378d01b2e
cihatkama <cihatkama@gmail.com>
2020-05-25 23:58:21 +0300

    Updated working codes.

--- HW06/src/employee/Teller.java
+++ HW06/src/employee/Teller.java
@@ -1,5 +1,9 @@
 package employee;
 
+//import static commerce.Bank;
+// import static commerce.Bank.*;
+//import static commerce.Bank;
+
 public class Teller {
 	public void processAccount(int depositSlip, int withdrawalSlip) {
 //		withdrawal(withdrawalSlip);
--- HW06/src/tr/com/ckama/Q44.java
+++ HW06/src/tr/com/ckama/Q44.java
@@ -2,15 +2,18 @@ package tr.com.ckama;
 
 public class Q44 {
 	public final static long numberOfTrees;
-	public static double height;
+	public final double height;
+
 	static {
 	}
+
 	{
 		final int initHeight = 2;
 		height = initHeight;
 	}
+
 	static {
 		numberOfTrees = 100;
-		height = 4;
+//		height = 4;
 	}
 }
